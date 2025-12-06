37c1e800fa6e1d706e48768ea80163562534391d
Jyoti <123319919+21Jyoti21@users.noreply.github.com>
2025-12-06 12:17:10 +0530

    Refactor query loop and clean up comments
    Removed comments and adjusted the loop for querying Amazon.

--- project.py
+++ project.py
@@ -6,10 +6,8 @@ import time
 driver = webdriver.Firefox()
 query="laptop"
 file=0
-#ek se lekar 20 page pe jitti queries hongi wo dega
 for i in range(1,20):
     driver.get(f"https://www.amazon.in/s?k={query}&page={i}&crid=3C5P82EHCC7AR&sprefix=lap%2Caps%2C518&ref=nb_sb_noss_2")
-    # https://www.amazon.in/s?k=laptop&page=2&xpid=Z3QQNHmBorxL9&crid=3C5P82EHCC7AR&qid=1757339511&sprefix=lap%2Caps%2C518&ref=sr_pg_2
     elems = driver.find_elements(By.CLASS_NAME, "puis-card-container")
     print(f"{len(elems)} items found")
     for elem in elems:
