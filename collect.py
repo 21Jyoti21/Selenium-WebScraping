6c160ff9402af9d0f42d9ca132f513bc41fa931b
Jyoti <123319919+21Jyoti21@users.noreply.github.com>
2025-12-06 12:18:34 +0530

    Update collect.py

--- collect.py
+++ collect.py
@@ -1,25 +1,3 @@
-# from bs4 import BeautifulSoup
-# import os
-
-# d={'title':[1,2],'price':[3,4],'link':[1,2]}
-
-# for file in os.listdir("data"):
-#     with open(f"data/{file}") as f:
-#         html_doc=f.read()
-#     soup = BeautifulSoup(html_doc, 'html.parser')
-#     t=soup.find("h2")
-#     if not t:
-#         continue
-#     title=t.get_text()
-#     print(title)
-
-#     l=t.find("a")
-#     link="https://amazon.in/"+l['href'] if l and l.has_attr("href") else None
-#     print(title,link)
-#     break
-#     # print(soup.prettify())
-
-
 from bs4 import BeautifulSoup
 import os
 import pandas as pd
@@ -32,11 +10,9 @@ for file in os.listdir("data"):
 
         soup = BeautifulSoup(html_doc, 'html.parser')
 
-        # Title
         title_tag = soup.find("span", {"class": "a-size-medium"})
         title = title_tag.get_text(strip=True) if title_tag else "N/A"
 
-        # Link
         link_tag = soup.find("a", {"class": "a-link-normal"})
         link = "https://www.amazon.in" + link_tag["href"] if link_tag and link_tag.has_attr("href") else "N/A"
 
@@ -46,8 +22,7 @@ for file in os.listdir("data"):
         d['title'].append(title)
         d['price'].append(price)
         d['link'].append(link)
-        # print(title, link ,price)
-        # break
+       
     except Exception as e:
         print(e)
 df=pd.DataFrame(data=d)
